0ed5f5e2597c3a445f05db61f8f38970c7cf0830
christinoronaldo <74182563+christinoronaldo@users.noreply.github.com>
2024-03-19 14:48:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-const firebase = require('firebase/app');
-const firestore = require('firebase/firestore');
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 const port = 3000;
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC4WTaRLaw_QsL9RkphbAgWyYgta7rPsDw",
-  authDomain: "distance1-306c8.firebaseapp.com",
-  databaseURL: "https://distance1-306c8-default-rtdb.firebaseio.com",
-  projectId: "distance1-306c8",
-  storageBucket: "distance1-306c8.appspot.com",
-  messagingSenderId: "793547982904",
-  appId: "1:793547982904:web:e01c986d18b4e1f6ce7ecd",
-  measurementId: "G-BLLHS69S34"
-};
-
-firebase.initializeApp(firebaseConfig);
-console.log('Firestore instance:', firestore);
-
+app.set('view engine', 'ejs');
 app.use(express.json());
-
-let short = [];
 
 app.get('/', (req, res) => {
   res.sendFile('homepage.html',{ root: __dirname });
@@ -30,12 +14,18 @@ app.get('/.well-known/assetlinks.json', (req, res) => {
   res.sendFile('.well-known/assetlinks.json',{ root: __dirname });
 });
 
-app.get('/1', (req, res) => {
-  res.sendFile('search.html',{ root: __dirname });
-})
+app.get('/reel', (req, res) => {
+  const username = req.query.param1;
+  const audio = req.query.param2;
+  const videoUrl = req.query.param3;
+  const title = req.query.param4;
+  const postOn = req.query.param5;
+  const likes = req.query.param6;
+  const comments = req.query.param7;
 
-app.get('/api/recive', (req, res) => {
-  res.json(short);
+  app.get('/username/audio/videoUrl/title/postOn/', (reqNested, resNested) => {
+    resNested.render('distance', { un: username, aud: audio, vu: videoUrl, t: title, p:postOn, l:likes, c: comments});
+  });
 });
 
 app.listen(port, () => {
